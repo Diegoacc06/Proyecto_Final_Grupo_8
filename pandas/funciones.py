3b42bd0115ef91dc74259a426d83ee41d2439d58
Diegoacc06 <diego.cascante@est.uexternado.edu.co>
2025-11-21 05:42:43 +0000

    Creación función promedios, Diego Cascante

---
+++ pandas/funciones.py
@@ -0,0 +1,9 @@
+def promedios(nombre_archivo,nombre_columna):
+    try:
+        if nombre_columna in nombre_archivo.columns:
+            promedio= nombre_archivo[nombre_columna].mean()
+            return  f"El promedio de la columna {nombre_columna}, es: {promedio}."
+        else:
+            return "Error al procesar la columna."
+    except Exception:
+        return "Error al procesar la columna."
